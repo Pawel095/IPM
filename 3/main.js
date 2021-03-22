303aa1d7b7688cc6aff26cce7be962552719e746
Paweł Pankiewicz <pawelp6020@gmail.com>
2021-03-22 11:08:15 +0100

    Canvas is not fuzzy anymore

--- 3/main.js
+++ 3/main.js
@@ -1,23 +1,61 @@
+// globals
+var blocks = new Array();
+var btn;
+var cnv;
+var ctx;
+
 window.onload = () => {
-    class Rect {
-        constructor(w, h) {
-            this.width = w;
-            this.height = h;
-            this.x = 0;
-            this.y = 0;
+    btn = document.getElementById('spawn-element');
+    cnv = document.getElementById('canvas');
+    ctx = cnv.getContext('2d');
+    w = window.innerWidth;
+    h = window.innerHeight * 0.98;
+    ctx.canvas.width = w;
+    ctx.canvas.height = h;
+
+    class Square {
+        constructor(size) {
+            this.size = size;
+            this.x = 100;
+            this.y = 100;
+            this.color =
+                'rgb(' +
+                Math.floor(Math.random() * 255) +
+                ',' +
+                Math.floor(Math.random() * 255) +
+                ',' +
+                Math.floor(Math.random() * 255) +
+                ')';
         }
-        isInside(x,y) {
-            
+
+        isInside(x, y) {
+            var xIn = true;
+            var yIn = true;
+
+            if (this.x > x || this.x + this.size < x) xIn = false;
+            if (this.y > y || this.y + this.size < y) yIn = false;
+
+            return xIn && yIn;
+        }
+        draw() {
+            ctx.fillStyle = this.color;
+            ctx.fillRect(this.x, this.y, this.size, this.size);
         }
     }
 
-    var blocks = new Array();
+    // util functions
+    function spawnBlock(x, y) {
+        blocks.push(new Square(w * 0.02));
+    }
 
-    var btn = document.getElementById('spawn-element');
-    var cnv = document.getElementById('canvas');
-    var ctx = cnv.getContext('2d');
+    // event listeners
+    btn.addEventListener('click', (event) => {
+        spawnBlock(100, 100);
+        blocks.forEach((block, index) => {
+            block.draw();
+        });
+    });
 
-    function spawnBlock(x, y, w, h) {}
-
-    function draw() {}
+    // Drag and drop handler
+    
 };
