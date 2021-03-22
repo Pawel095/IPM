9a581fed77e88425b457c60a8dda1e621e753eba
Paweł Pankiewicz <pawelp6020@gmail.com>
2021-03-22 10:23:27 +0100

    początki kodu do dodawania kwadratów

--- 3/main.js
+++ 3/main.js
@@ -1,4 +1,23 @@
 window.onload = () => {
+    class Rect {
+        constructor(w, h) {
+            this.width = w;
+            this.height = h;
+            this.x = 0;
+            this.y = 0;
+        }
+        isInside(x,y) {
+            
+        }
+    }
+
+    var blocks = new Array();
+
     var btn = document.getElementById('spawn-element');
     var cnv = document.getElementById('canvas');
+    var ctx = cnv.getContext('2d');
+
+    function spawnBlock(x, y, w, h) {}
+
+    function draw() {}
 };
